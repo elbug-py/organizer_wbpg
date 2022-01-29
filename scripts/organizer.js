a3e4f652437e7441e56137480b9e81551187b7e9
vicente romero <vbromero@miuandes.cl>
2022-01-29 03:12:09 -0300

    Now chores can be move with buttons :D

--- scripts/organizer.js
+++ scripts/organizer.js
@@ -1,3 +1,5 @@
+state_arr = ['To-do', 'Ongoing', 'Finished', 'Dropped', 'Stand-by']
+
 
 $("#submit").on('click',function(){
     submit();
@@ -10,7 +12,40 @@ $(document).on('click', '#danger', function(){
 $(document).on('click', "#add", function(){
     let state = $(this).closest('div').siblings('ul').attr('id');
     let chore = $(this).siblings('input').val(); 
-    $("#"+state).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button></li>');
+    $("#"+state).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button><button class="btn btn-warning btn-xs" id="left"><</button><button class="btn btn-warning btn-xs" id="right">></button></li>');
     $(this).siblings('input').val("");
 });
 
+$(document).on('click', '#right', function(){
+    let state = $(this).closest('ul').attr('id');
+    let idx = state_arr.indexOf(state) + 1;
+    let chore = $(this).closest("li").text().slice(0,-3);
+    if ( idx < state_arr.length){
+        console.log(chore)
+        $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { $(this).remove(); });
+        $("#"+state_arr[idx]).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button><button class="btn btn-warning btn-xs" id="left"><</button><button class="btn btn-warning btn-xs" id="right">></button></li>');
+
+    }
+    else{
+        //$(this).fadeOut('slow', function() { $(this).hide; });
+        console.log('te pasaste')
+    }
+    
+});
+
+$(document).on('click', '#left', function(){
+    let state = $(this).closest('ul').attr('id');
+    let idx = state_arr.indexOf(state) - 1;
+    let chore = $(this).closest("li").text().slice(0,-3);
+    if ( idx >= 0){
+        console.log(chore)
+        $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { $(this).remove(); });
+        $("#"+state_arr[idx]).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button><button class="btn btn-warning btn-xs" id="left"><</button><button class="btn btn-warning btn-xs" id="right">></button></li>');
+
+    }
+    else{
+        //$(this).fadeOut('slow', function() { $(this).hide; });
+        console.log('te pasaste')
+    }
+    
+});
