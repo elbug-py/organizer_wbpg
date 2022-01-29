ed81eba8397ebe02780cbd2a7b3b57b3f37aa8db
vicente romero <vbromero@miuandes.cl>
2022-01-29 02:38:09 -0300

    now the js is all in jquery and way more effcient to read yay!

--- scripts/organizer.js
+++ scripts/organizer.js
@@ -1,72 +1,16 @@
 
-function submit(){
-
-    let chore = document.getElementById('chore').value;
-    var select = document.getElementById('state');
-	var option = select.options[select.selectedIndex].text;
-    var ul = document.getElementById(option);
-    var li = document.createElement("li");
-    let btn = document.createElement("button");
-    btn.className = "btn btn-danger btn-xs"
-    btn.id = "danger"
-    let ulElem = document.getElementById(option);
-    let nodes = ulElem.childNodes
-    
-    for (const li of nodes){
-        if (li.nodeName != '#text'){
-            if (li.innerHTML == chore + '<button class="btn btn-danger btn-xs" id="danger">x</button>'){
-                alert('This Chore Already Exists, Try Again')
-                return false
-            }
-        console.log(li.innerHTML)
-        }
-    } 
-
-    btn.appendChild(document.createTextNode('x'))
-    li.appendChild(document.createTextNode(chore));
-    li.appendChild(btn)
-    $("#"+option).prepend(li);
-    //ul.appendChild(li);
-}
-
-
 $("#submit").on('click',function(){
     submit();
   });
-
-
-/*function remove(){
-    let chore = document.getElementById('chore').value;
-    let select = document.getElementById('state_del');
-	let option = select.options[select.selectedIndex].text;
-    let ulElem = document.getElementById(option);
-    let nodes = ulElem.childNodes
-    
-    for (const li of nodes){
-        if (li.nodeName != '#text'){
-            if (li.innerHTML == chore + '<button>x</button>'){
-                ulElem.removeChild(li)
-                return true
-            }
-        }
-    } 
-    alert('Item not found, Try Again')
-}
-*/
-
-
-
 
 $(document).on('click', '#danger', function(){
     $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { $(this).remove(); });
 });
 
-$(document).on('click', '#add', function(){
-    submit();
+$(document).on('click', "#add", function(){
+    let state = $(this).closest('div').siblings('ul').attr('id');
+    let chore = $(this).siblings('input').val(); 
+    $("#"+state).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button></li>');
+    $(this).siblings('input').val("");
 });
 
-addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      submit()
-    }
-});
