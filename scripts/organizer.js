dfa22e00627ab700f92b0e7c311c74840c2dc147
vicente romero <vbromero@miuandes.cl>
2022-01-29 23:41:54 -0300

    Now it uses the local storage to save changes when the page is reload or closed

--- scripts/organizer.js
+++ scripts/organizer.js
@@ -21,14 +21,12 @@ $(document).on('click', '#right', function(){
     let idx = state_arr.indexOf(state) + 1;
     let chore = $(this).closest("li").text().slice(0,-3);
     if ( idx < state_arr.length){
-        console.log(chore)
-        $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { $(this).remove(); });
+        $(this).closest('li').fadeOut('slow', function() { $(this).remove(); });
         $("#"+state_arr[idx]).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button><button class="btn btn-warning btn-xs" id="left"><</button><button class="btn btn-warning btn-xs" id="right">></button></li>');
 
     }
     else{
-        //$(this).fadeOut('slow', function() { $(this).hide; });
-        console.log('te pasaste')
+        $(this).fadeOut('slow', function() { $(this).hide; });
     }
     
 });
@@ -38,14 +36,95 @@ $(document).on('click', '#left', function(){
     let idx = state_arr.indexOf(state) - 1;
     let chore = $(this).closest("li").text().slice(0,-3);
     if ( idx >= 0){
-        console.log(chore)
-        $(this).closest('li').toggleClass('strike').fadeOut('slow', function() { $(this).remove(); });
+        $(this).closest('li').fadeOut('slow', function() { $(this).remove(); });
         $("#"+state_arr[idx]).append('<li>'+chore+'<button class="btn btn-danger btn-xs" id="danger">x</button><button class="btn btn-warning btn-xs" id="left"><</button><button class="btn btn-warning btn-xs" id="right">></button></li>');
 
     }
     else{
-        //$(this).fadeOut('slow', function() { $(this).hide; });
-        console.log('te pasaste')
+        $(this).fadeOut('slow', function() { $(this).hide; });
     }
     
 });
+
+
+
+
+/*$(document).on('click', '#user-btn', function(){
+    if (localStorage.username) {
+        document.getElementById("demo").innerHTML = localStorage.username;
+      } else {
+        localStorage.username = $(this).siblings('input').val();
+      }
+    
+    console.log(user)
+    $(this).siblings('input').val("");
+});*/
+
+$("#Username").text(localStorage.username);
+
+function changeUser(){
+    if ($("#user").val() == '') {
+
+        console.log(localStorage.username)
+      } else {
+        localStorage.username = $("#user").val()
+        document.getElementById("Username").innerHTML = localStorage.username;
+      }
+    
+      $("#user").val("");
+}
+
+
+$(document).on('click', '#save', function(){
+
+    let state = $(this).siblings('ul').attr('id');
+    ul = document.getElementById(state).children
+    
+    chore_arr = []
+    for (li of ul){
+        if (chore_arr.includes(li)== false){
+        chore_arr.push(li.innerHTML)
+        }
+    }
+
+    if (chore_arr.length >= 1){
+    let myJSON = JSON.stringify(chore_arr)
+    localStorage.setItem(state, myJSON);
+    }
+});
+
+
+$(document).on('click', '#load', function(){
+
+    let state = $(this).siblings('ul').attr('id');
+    let text = localStorage.getItem(state);
+    let obj = JSON.parse(text);
+
+    for (chore of obj){
+        $("#"+state).append('<li>'+chore+'</li>');
+        }
+    
+
+
+});
+
+
+$(document).on('click', '#clear', function(){
+
+    let state = $(this).siblings('ul').attr('id');
+    localStorage.setItem(state, '');
+
+});
+
+
+for (state of state_arr){
+    try{
+    let text = localStorage.getItem(state);
+    let obj = JSON.parse(text);
+    for (chore of obj){
+        $("#"+state).append('<li>'+chore+'</li>');
+        }
+    } catch (error){
+        console.log(error)
+    }
+}
